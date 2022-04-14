4e1c8bd3c8d11067b683e15d82f5fa19739d4f5e
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-04-14 11:31:11 +0200

    multiple language support
    multiple language support (NL & EN)

--- cmd/addons/notification.js
+++ cmd/addons/notification.js
@@ -1,48 +1,50 @@
 const discord = require("discord.js");
 //File server
 const fs = require("fs");
-//Taal van de bot
+//Addon Setings
 const notification = JSON.parse(fs.readFileSync(`./src/addons/notification.json`, "utf-8"));
+//Language Seting
+const language = process.env.LANGUAGE
 
 module.exports.run = async (client, message, args) => {
 
-    if (!message.member.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply(`${language.no_admin}`).then(msg => {
+    if (!message.member.roles.cache.has(`${process.env.ADMINROLL}`)) return message.reply(`${notification.language.language.no_admin}`).then(msg => {
         message.delete()
         setTimeout(() => msg.delete(), 10000);
     });
 
     const msg_notification = args.splice(0,args.length).join(" ");
 
-    if(!msg_notification) return message.reply(`**${notification.no_msg}**`).then(msg => {
+    if(!msg_notification) return message.reply(`**${notification.language.no_msg}**`).then(msg => {
         message.delete()
         setTimeout(() => msg.delete(), 10000);
     });
 
     const notificationChannel = message.member.guild.channels.cache.get(notification.channel);
 
-    if(!notificationChannel) return message.reply(`${notification.no_channel}`).then(msg => {
+    if(!notificationChannel) return message.reply(`${notification.language.no_channel}`).then(msg => {
         message.delete()
         setTimeout(() => msg.delete(), 10000);
     });
 
     const Embed = new discord.MessageEmbed()
-    .setTitle(`${notification.title} ${message.member.displayName}`)
+    .setTitle(`${notification.language.title} ${message.member.displayName}`)
     .setFooter(message.member.displayName, message.author.displayAvatarURL)
     .setColor(process.env.COLLOR)
     .setThumbnail(process.env.LOGO)
     .setImage(process.env.BANNER)
     .setTimestamp()
-    .addField(`${notification.msg}`, `${msg_notification}`);
+    .addField(`${notification.language.msg}`, `${msg_notification}`);
 
     const sucsesEmbed = new discord.MessageEmbed()
-    .setTitle(`${notification.title} ${message.member.displayName}`)
+    .setTitle(`${notification.language.title} ${message.member.displayName}`)
     .setFooter(message.member.displayName, message.author.displayAvatarURL)
     .setColor(process.env.COLLOR)
     .setThumbnail(process.env.LOGO)
     .setImage(process.env.BANNER)
     .setTimestamp()
-    .setFooter(`${notification.footer}`)
-    .addField(`${notification.notification_chat}`, `${notificationChannel}`)
+    .setFooter(`${notification.language.footer}`)
+    .addField(`${notification.language.notification_chat}`, `${notificationChannel}`)
 
     message.channel.send({ embeds: [sucsesEmbed] }).then(msg => {
         message.delete()
@@ -57,5 +59,5 @@ module.exports.run = async (client, message, args) => {
 module.exports.help = {
     name: "notification",
     category: "admin",
-    discription: notification.cmd_notification_disc
+    discription: notification.language.cmd_notification_disc
 }
